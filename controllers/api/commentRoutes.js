4a1fcdb1d6dd8b447a94997d557b9aa991819b41
unknown <lawthouaxiong1750@gmail.com>
2023-06-21 19:32:05 -0500

    some routing work

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,38 +1,36 @@
 const router = require('express').Router();
-const { Commnet } = require('../../models');
-const withAuth = require('../../utils/auth');
+const { Comment, User } = require('../../models');
+const withAuth = require("../../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
-  try {
-    const newComment = await Comment.create({
-      ...req.body,
-      user_id: req.session.user_id,
-    });
+router.post('/', withAuth, async(req,res) => {
+    try {
+        const newComment = await Comment.create({
+            user_id: req.session.user_id,
+            comment_content: req.body.comment_content
+        });
 
-    res.status(200).json(newComment);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
+        res.status(200).json(newComment);
 
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const commentData = await Comment.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-    });
-
-    if (!commentData) {
-      res.status(404).json({ message: 'No comment found with this id!' });
-      return;
+    } catch(err) {
+        res.status(500).json(err);
     }
-
-    res.status(200).json(commentData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
 });
+
+router.delete("/:id", async (req, res) => {
+    try {
+      const comment = await Comment.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!comment) {
+        res.status(404).json({ message: "No comment found with that id!" });
+        return;
+      }
+      res.status(200).json(comment);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
