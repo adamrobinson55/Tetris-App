a1d19e73d4b0ab3f5b3f5235901d13d042ba873d
unknown <lawthouaxiong1750@gmail.com>
2023-06-21 19:32:05 -0500

    some routing work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
+const router = require("express").Router();
+const userRoutes = require("./userRoutes");
+const commentRoutes = require('./commentRoutes.js');
 
-router.use('/users', userRoutes);
+router.use("/users", userRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
